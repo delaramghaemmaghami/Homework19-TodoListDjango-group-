d0939cd73658475c1891a1cfee3730e6ce4e8110
DelaramGhaemmaghami <delaramsadatghaemmaghami1997@gmail.com>
2021-12-09 20:46:23 +0330

    models.py

--- todo/models.py
+++ todo/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=100, blank=False)
+    category_name = models.CharField(max_length=100,
+                                     blank=False)
+
     description = models.TextField(max_length=500)
 
     def __str__(self):
@@ -10,16 +12,23 @@ class Category(models.Model):
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=100, blank=False)
-    description = models.TextField(max_length=500, blank=False)
-    category = models.ManyToManyField(Category, related_name="cat_rel")
+    title = models.CharField(max_length=100,
+                             blank=False)
+
+    description = models.TextField(max_length=500,
+                                   blank=False)
+
+    category = models.ManyToManyField(Category,
+                                      related_name="cat_rel")  # views.py ->class CategoryList ->Most, Empty
 
     PRIORITY_CHOICES = [
         ('high', 'high'),
         ('medium', 'medium'),
         ('low', 'low')
     ]
-    priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES, default="low")
+    priority = models.CharField(max_length=6,
+                                choices=PRIORITY_CHOICES,
+                                default="low")
 
     create_time = models.DateField(auto_now_add=True)
     dead_line = models.DateField()
